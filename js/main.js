7bfd32b5579cdde144b48fffb87749c355a63ba1
AlexSeitov <seitov_ai@mail.ru>
2020-07-22 17:55:40 +0300

    сверстал фильтры

--- js/main.js
+++ js/main.js
@@ -34,4 +34,36 @@ $(function () {
         prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt="arrow left"></button>',
         nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt="arrow right"></button>',
     });
+
+    // formstyler
+
+    $('.filter-style').styler();
+
+    // drop down list
+
+    $('.filter__item-drop, .filter-extra').on('click', function() {
+        $(this).toggleClass('filter__item-drop--active');
+        $(this).next().slideToggle('200');
+    });
+
+    // ionRangeSlide
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        grid: false,
+        min: 100000,
+        max: 500000,
+    });
+
+    $('.catalog__filter-btn-grid').on('click', function() {
+        $(this).addClass('catalog__filter-buttons--active');
+        $('.catalog__filter-btn-line').removeClass('catalog__filter-buttons--active');
+        $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+    });
+
+    $('.catalog__filter-btn-line').on('click', function() {
+        $(this).addClass('catalog__filter-buttons--active');
+        $('.catalog__filter-btn-grid').removeClass('catalog__filter-buttons--active');
+        $('.product-item__wrapper').addClass('product-item__wrapper--list');
+    });
 });
