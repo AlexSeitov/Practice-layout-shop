d780907e729559169f9d92d8e9e1740be16e7b6a
AlexSeitov <seitov_ai@mail.ru>
2020-07-24 10:07:07 +0300

    сделал адаптив слайдера и баннера

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ $(function () {
         dots: true,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt="arrow left"></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt="arrow right"></button>',
+        responsive: [
+            {
+              breakpoint: 969,
+              settings: {
+                arrows: 'false'
+              }
+            }
+          ]
     });
 
     // tabs
